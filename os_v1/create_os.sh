aec76f338369358cfd0eb9acfea14aa1695e369f
arjuncr <arjuncr00@gmail.com>
2019-07-02 22:34:28 +0530

    fix adding tmp folder

--- os_v1/create_os.sh
+++ os_v1/create_os.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+if [ -d tmp ]
+then
+	rm -r tmp
+fi
+
+mkdir tmp
+mkdir -p iso/boot/grub
 
 nasm -f elf32 src/loader.s -o tmp/loader.o
-
 ld -T src/link.ld -melf_i386 tmp/loader.o -o tmp/kernel.elf
 
-
-mkdir -p iso/boot/grub              # create the folder structure 
 cp grub/stage2_eltorito iso/boot/grub/   # copy the bootloader
 cp grub/menu.lst       iso/boot/grub    #copy bootloader configuration
 cp tmp/kernel.elf iso/boot/             # copy the kernel
